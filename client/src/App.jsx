e1a6826914b074ade9aa6334954e7921b2dbd017
Surendra Yadav <dev.filldesigngroup@gmail.com>
2026-02-20 17:09:28 +0530

    printed consoldle log

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
 import { useState, useEffect } from 'react'
-
 import './App.css'
 
 function App() {
+  console.log("VITE_API_URL =", import.meta.env.VITE_API_URL)
   const [message, setMessage] = useState(0)
 
   useEffect(() => {
-  fetch(`http://76.13.242.148:4000/api/message`)
+    fetch(`${import.meta.env.VITE_API_URL}/api/message`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch((err) => {
-        console.error("error fetching message")
+        console.error("error fetching message", err)
       })
   }, [])
 
   return (
     <>
       <h1>Hello SEBMTG</h1>
-      <p>
-        {message}
-      </p>
+      <p>{message}</p>
     </>
   )
+  
 }
 
 export default App
